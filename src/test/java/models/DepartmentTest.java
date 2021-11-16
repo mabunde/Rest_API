a042afcb67b9fccd05be6ac42c419199e8fdd343
mabunde <solomonsitati39@gmail.com>
2021-11-16 20:02:19 +0300

    Add department test

--- src/test/java/models/DepartmentTest.java
+++ src/test/java/models/DepartmentTest.java
@@ -1,7 +1,41 @@
 package models;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class DepartmentTest {
+    @Test
+    public void department_instantiatesCorrectly_true() {
+        Department department1 = new Department("IT", "networking");
+        assertTrue(department1 instanceof Department);
+    }
 
+    @Test
+    public void description_instantiatesWithDescription_description() {
+        Department department = createDepartment();
+        assertEquals("Generate payroll", department.getDescription());
+    }
+    @Test
+    public void departmentName_setsName_name() {
+        Department department = createDepartment();
+        department.setName("Social");
+        assertEquals("Social", department.getName());
+    }
+    @Test
+    public void description_setsDescription_description() {
+        Department department = createDepartment();
+        department.setDescription("networking");
+        assertEquals("networking", department.getDescription());
+    }
+    @Test
+    public void name_instantiatesWithName_name() {
+        Department department1 = createDepartment();
+        assertEquals("finance", department1.getName());
+    }
+
+    // HELPERS
+    public Department createDepartment(){
+        return new Department("finance", "Generate payroll");
+    }
 }
