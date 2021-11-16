f27b1793c1ea063ad81e84a21a17cd808fc8f697
mabunde <solomonsitati39@gmail.com>
2021-11-16 14:25:25 +0300

    Add route to retrieve all departments

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,19 @@
+import com.google.gson.Gson;
+import static spark.Spark.*;
+
+import dao.*;
+import exceptions.ApiException;
+import models.*;
 public class App {
+    public static void main(String[] args) {
+        Sql2oDepartmentDao departmentDao = new Sql2oDepartmentDao();
+        Sql2oUserDao employeeDao = new Sql2oUserDao();
+        Sql2oNewsDao newsDao = new Sql2oNewsDao();
+
+        Gson gson = new Gson();
+        // get all departments
+        get("/departments", "application/json", (request, response) -> {
+            return gson.toJson(departmentDao.allDepartments());
+        });
+    }
 }
