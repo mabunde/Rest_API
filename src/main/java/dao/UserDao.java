bddea451aeb336e5fea93fff34a36419a0ddccc6
mabunde <solomonsitati39@gmail.com>
2021-11-16 12:01:46 +0300

    create dao files

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -1,4 +1,23 @@
 package dao;
 
+import models.User;
+import models.News;
+
+import java.util.List;
+
 public interface UserDao {
+    // create
+    void add(User employee);
+
+    // read
+    List<User> getAllEmployees();
+    User findById(int id);
+    List<News> userNews(int userId);
+
+    // update
+
+
+    // delete
+    void deleteById(int id);
+    void clearAll();
 }
--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,4 +1,48 @@
 package models;
 
 public class User {
+    private int id;
+    private String name;
+    private String position;
+    private int departmentId;
+
+    public User(int id, String name, String position,int departmentId) {
+        this.id = id;
+        this.name = name;
+        this.position = position;
+        this.departmentId = departmentId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
+
 }
